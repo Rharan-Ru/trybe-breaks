e18e254cb04cdec9e9b7548662e1ff67fa78884a
Rharan-Ru <rharanru@gmail.com>
2022-02-27 21:59:47 -0300

    Add key press listener to activate functions like play/pause

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,5 @@
+// Create Youtube embed player control
+
 var tag = document.createElement('script');
 tag.id = 'iframe-demo';
 tag.src = 'https://www.youtube.com/iframe_api';
@@ -13,23 +15,42 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
-function playVideo() {
-    var play = document.getElementById('play-video');
-    play.innerHTML = '<i class="fa fa-pause"></i>';
-    play.id = 'pause-video';
-    play.setAttribute('onclick', 'pauseVideo()');
-    player.playVideo();
-}
+// Video buttons control
+var paused = true;
 
-function pauseVideo() {
-    var play = document.getElementById('pause-video');
+function playPauseVideo() {
+    var play = document.getElementById('play-pause-video');
+    if (paused) {
+        play.innerHTML = '<i class="fa fa-pause"></i>';
+        player.playVideo();
+        paused = false;
+        return paused;
+    }
     play.innerHTML = '<i class="fa fa-play"></i>';
-    play.id = 'play-video';
-    play.setAttribute('onclick', 'playVideo()');
     player.pauseVideo();
+    paused = true;
+    return paused;
 }
 
 
 function setVolume(event) {
-    player.setVolume(event)
+    player.setVolume(event);
+    return;
 }
+
+// Activate functions in keyboard press
+
+document.onkeydown = keyPressed;
+
+function keyPressed(k) {
+    console.log(k);
+    var pause = 80;
+    var random = 82;
+    var next = 65;
+    var prev = 68;
+    switch (k.keyCode) {
+        case pause:
+            playPauseVideo();
+            return;
+    }
+};
